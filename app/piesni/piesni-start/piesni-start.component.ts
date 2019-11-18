a16e56336df130a360f0800df0149761ba990928
Micharz <rzutek7@tlen.pl>
2019-11-18 18:02:31 +0100

    Add files via upload

---
+++ app/piesni/piesni-start/piesni-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-piesni-start',
+  templateUrl: './piesni-start.component.html',
+  styleUrls: ['./piesni-start.component.css']
+})
+export class PiesniStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
