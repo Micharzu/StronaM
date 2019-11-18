58036c5b14252cbdae390dd332e48592efd647ff
Micharz <rzutek7@tlen.pl>
2019-11-18 18:05:47 +0100

    Add files via upload

---
+++ app/zespol/zespol.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zespol',
+  templateUrl: './zespol.component.html',
+  styleUrls: ['./zespol.component.css']
+})
+export class ZespolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
